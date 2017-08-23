63d362d74d85f15c9c21f5c58474085c2f1b593f
marynganga <marynganga.mw@gmail.com>
2017-08-23 16:08:16 +0300

    Update scripts.js 1.Added form-control class to the select tag on appending.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,8 @@ function Contact(first, last) {
     this.addresses = []; //leave it as an empty array since we will push the the address object created into it.
 };
 //Address Constructor
-function Address(street, city, county) {
+function Address(addressType, street, city, county) {
+    this.addressType = addressType;
     this.street = street;
     this.city = city;
     this.county = county;
@@ -18,7 +19,7 @@ Contact.prototype.fullName = function () {
 }
 //full address prototype method
 Address.prototype.fullAddress = function () {
-    return this.street + ', ' + this.city + ', ' + this.county;
+    return this.addressType + ": " + this.street + ', ' + this.city + ', ' + this.county;
 }
 // Clear the input fields after submission
 function resetFields() {
@@ -27,6 +28,7 @@ function resetFields() {
     $("input.new-street").val("");
     $("input.new-city").val("");
     $("input.new-county").val("");
+    $("select.address-type").val("None");
 }
 //FRONT END LOGIC
 $(document).ready(function () {
@@ -36,13 +38,13 @@ $(document).ready(function () {
         $('#new-addresses').append(
             '<div class="form-group">' +
             '<label for="address-type">Choose address type</label>' +
-            '<select required name="address-type" form="new-contact" class="address-type">' +
+            '<select required name="address-type" form="new-contact" class="address-type form-control">' +
             '<option value="">None</option>' +
             '<option value="home">Home</option>' +
-            '<option value ="work">Work</option>'+ 
-            '<option value ="mailing">Mailing</option>'+ 
-            '</select>'+ 
-            '</div>'+
+            '<option value ="work">Work</option>' +
+            '<option value ="mailing">Mailing</option>' +
+            '</select>' +
+            '</div>' +
             '<div class="new-address">' +
             '<div class="form-group">' +
             '<label for="new-street">Street</label>' +
@@ -68,16 +70,20 @@ $(document).ready(function () {
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
         // looping through the address form fields to collect the information inserted, create a newAddress object with it and pushing the newAddress object to the addresses property array in the newContact object.
         $('.new-address').each(function () {
+            var inputtedAddressType = $(this).find("select.address-type option:checked").val();
+            //var e = $(this).find("select.address-type");
+           // var inputtedAddressType = e.options[e.selectedIndex].value;
+            
             var inputtedStreet = $(this).find("input.new-street").val();
             var inputtedCity = $(this).find("input.new-city").val();
             var inputtedCounty = $(this).find("input.new-county").val();
-            var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
+            var newAddress = new Address(inputtedAddressType, inputtedStreet, inputtedCity, inputtedCounty);
             newContact.addresses.push(newAddress);
         });
 
         //display the contact's fullnames
         $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
-
+        //display inputted information on clicking the contact's full names
         $(".contact").last().click(function () {
             $("#show-contact").show();
             $("#show-contact h2").text(newContact.fullName());
